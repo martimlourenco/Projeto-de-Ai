63e825c83a2391cbf66c17d758d026a437b35a0d
Tomas Azoia <tomasazoiamorais@gmail.com>
2024-01-22 14:34:26 +0000

    tempo JS

---
+++ time.js
@@ -0,0 +1,15 @@
+time();
+function time()
+{
+    const interval = setInterval(function() {
+        var today = new Date();
+        var hour = today.getHours();
+        var minute = today.getMinutes();
+        var second = today.getSeconds();
+        if(hour<10) hour = "0" + hour;
+        if(minute<10) minute = "0" + minute;
+        if(second<10) second = "0" + second;
+        var time = hour + ":" + minute + ":" + second;
+        document.getElementById("time").innerText = time;
+      }, 100);
+}
